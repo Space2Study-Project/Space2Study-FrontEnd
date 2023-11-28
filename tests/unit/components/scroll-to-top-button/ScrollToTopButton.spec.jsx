8e0d7b0711530850f20d6b47de207c9bbcbbc3a4
Leonid <leonideko1@gmail.com>
2023-11-28 11:17:55 +0200

    delete function scroll in navbar folder

--- tests/unit/components/scroll-to-top-button/ScrollToTopButton.spec.jsx
+++ tests/unit/components/scroll-to-top-button/ScrollToTopButton.spec.jsx
@@ -1,38 +1,7 @@
-// import { render, screen, fireEvent, waitFor } from '@testing-library/react'
-// import ScrollToTopButton from '~/components/scroll-to-top-button/ScrollToTopButton'
-
-// window.scrollTo = vi.fn()
-
-// describe('ScrollToTopButton dialog test', () => {
-//   beforeEach(() => {
-//     render(<ScrollToTopButton element={{ current: window }} />)
-//   })
-
-//   it('should show ArrowUpwardRoundedIcon', async () => {
-//     fireEvent.scroll(window, { target: { scrollTop: 500 } })
-
-//     await waitFor(() =>
-//       expect(screen.getByTestId('ArrowUpwardRoundedIcon')).toBeInTheDocument()
-//     )
-//   })
-//   it('should call function scrollTo', async () => {
-//     fireEvent.scroll(window, { target: { scrollTop: 500 } })
-
-//     const button = screen.queryByTestId('ArrowUpwardRoundedIcon')
-
-//     fireEvent.click(button)
-
-//     expect(window.scrollTo).toHaveBeenCalledWith({
-//       behavior: 'smooth',
-//       top: 0
-//     })
-//   })
-// })
 import { render, screen, fireEvent, waitFor } from '@testing-library/react'
 import ScrollToTopButton from '~/components/scroll-to-top-button/ScrollToTopButton'
-import { jest } from 'jest'
-// Mock scrollTo function
-window.scrollTo = jest.fn()
+
+window.scrollTo = vi.fn()
 
 describe('ScrollToTopButton dialog test', () => {
   beforeEach(() => {
@@ -46,18 +15,16 @@ describe('ScrollToTopButton dialog test', () => {
       expect(screen.getByTestId('ArrowUpwardRoundedIcon')).toBeInTheDocument()
     )
   })
-
   it('should call function scrollTo', async () => {
     fireEvent.scroll(window, { target: { scrollTop: 500 } })
 
-    const button = screen.getByTestId('ArrowUpwardRoundedIcon')
+    const button = screen.queryByTestId('ArrowUpwardRoundedIcon')
 
     fireEvent.click(button)
 
-    // Use jest's mock function to check if scrollTo is called with the expected arguments
     expect(window.scrollTo).toHaveBeenCalledWith({
-      top: 0,
-      behavior: 'smooth'
+      behavior: 'smooth',
+      top: 0
     })
   })
 })
