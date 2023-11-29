80b4aae4189343edf4af1bf2ecb82d65330feefe
Andrii Nasadiuk <123100628+AndriiNasadiuk@users.noreply.github.com>
2023-11-29 13:31:35 +0200

    next

--- src/pages/offer-details/OfferDetails.jsx
+++
@@ -1,7 +0,0 @@
-import PageWrapper from '~/components/page-wrapper/PageWrapper'
-
-const OfferDetails = () => {
-  return <PageWrapper>OfferDetails Page</PageWrapper>
-}
-
-export default OfferDetails
--- src/router/constants/studentRoutes.js
+++
@@ -1,9 +0,0 @@
-import { authRoutes } from '~/router/constants/authRoutes'
-
-export const studentRoutes = {
-  navBar: {
-    categories: { route: 'categories', path: authRoutes.categories.path },
-    howItWorks: { route: 'how-it-works', path: '/student/#how-it-works' },
-    faq: { route: 'faq', path: '/student/#faq' }
-  }
-}
