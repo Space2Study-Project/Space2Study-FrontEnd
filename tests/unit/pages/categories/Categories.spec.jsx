7cb4895686d982a1c4fef53cc36b361f8f5cabee
marianamartsy <mart7mariana@gmail.com>
2023-12-01 16:01:28 +0100

    tests to UT categories added

--- tests/unit/pages/categories/Categories.spec.jsx
+++ tests/unit/pages/categories/Categories.spec.jsx
@@ -1,11 +1,26 @@
-import { render, screen } from '@testing-library/react'
+import { render } from '@testing-library/react'
+import { vi } from 'vitest'
 import Categories from '~/pages/categories/Categories'
 
-describe('Categories page', () => {
-  it('should render the page whith the page text in the document', () => {
-    render(<Categories />)
-    const pageTextElement = screen.getByText('Categories')
+vi.mock('~/components/page-wrapper/PageWrapper', () => ({
+  __esModule: true,
+  default: function () {
+    return <div data-testid='categories'>Categories</div>
+  }
+}))
 
-    expect(pageTextElement).toBeInTheDocument()
+describe('Categories Page', () => {
+  it('should render the page', () => {
+    const { getByTestId } = render(<Categories />)
+    const pageWrapperCategory = getByTestId('categories')
+
+    expect(pageWrapperCategory).toBeInTheDocument()
+  })
+
+  it('should have text in the document', () => {
+    const { getByText } = render(<Categories />)
+    const categoriesText = getByText('Categories')
+
+    expect(categoriesText).toBeInTheDocument()
   })
 })
