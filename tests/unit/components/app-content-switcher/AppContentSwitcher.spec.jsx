1c62b116bc9926f8d629cff16b39f0b8dba92c88
Leonid <leonideko1@gmail.com>
2023-12-01 13:52:09 +0200

    add op provide switchOptions with only left property, only right property

--- tests/unit/components/app-content-switcher/AppContentSwitcher.spec.jsx
+++ tests/unit/components/app-content-switcher/AppContentSwitcher.spec.jsx
@@ -4,11 +4,14 @@ import { vi } from 'vitest'
 import AppContentSwitcher from '~/components/app-content-switcher/AppContentSwitcher'
 
 describe('AppContentSwitcher Component', () => {
-  const switchOptions = {
+  const switchOptionsLeft = {
     left: {
       text: 'left text',
       tooltip: 'left tooltip'
-    },
+    }
+  }
+
+  const switchOptionsRight = {
     right: {
       text: 'right text',
       tooltip: 'right tooltip'
@@ -20,12 +23,11 @@ describe('AppContentSwitcher Component', () => {
       <AppContentSwitcher
         active
         onChange={() => {}}
-        switchOptions={switchOptions}
+        switchOptions={switchOptionsLeft}
         typographyVariant='h6'
       />
     )
     expect(getByText('left text')).toBeInTheDocument()
-    expect(getByText('right text')).toBeInTheDocument()
     expect(getByTestId('switch')).toBeInTheDocument()
   })
   it('should call the onChange function when the switch is clicked', () => {
@@ -34,7 +36,7 @@ describe('AppContentSwitcher Component', () => {
       <AppContentSwitcher
         isStudent
         onChange={onChangeMock}
-        switchOptions={switchOptions}
+        switchOptions={switchOptionsLeft}
         typographyVariant='h6'
       />
     )
@@ -47,12 +49,50 @@ describe('AppContentSwitcher Component', () => {
       <AppContentSwitcher
         active
         onChange={() => {}}
-        switchOptions={switchOptions}
+        switchOptions={switchOptionsLeft}
         typographyVariant='h6'
       />
     )
     const leftTooltip = screen.getByText('left text')
     expect(leftTooltip).toBeInTheDocument()
+  })
+  it('should render correctly with props', () => {
+    const { getByText, getByTestId } = render(
+      <AppContentSwitcher
+        active
+        onChange={() => {}}
+        switchOptions={switchOptionsRight}
+        typographyVariant='h6'
+      />
+    )
+
+    expect(getByText('right text')).toBeInTheDocument()
+    expect(getByTestId('switch')).toBeInTheDocument()
+  })
+  it('should call the onChange function when the switch is clicked', () => {
+    const onChangeMock = vi.fn()
+    const { getByRole } = render(
+      <AppContentSwitcher
+        isStudent
+        onChange={onChangeMock}
+        switchOptions={switchOptionsRight}
+        typographyVariant='h6'
+      />
+    )
+    getByRole('checkbox').click()
+    expect(onChangeMock).toHaveBeenCalled()
+  })
+
+  it('should render tooltips if the tooltips props are passed', () => {
+    render(
+      <AppContentSwitcher
+        active
+        onChange={() => {}}
+        switchOptions={switchOptionsRight}
+        typographyVariant='h6'
+      />
+    )
+
     const rightTooltip = screen.getByText('right text')
     expect(rightTooltip).toBeInTheDocument()
   })
