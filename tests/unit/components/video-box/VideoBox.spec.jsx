b305fc1d99e9a00f42d984fdb9b7c33bd61eec0a
Leonid <leonideko1@gmail.com>
2023-12-01 12:45:06 +0200

    add test for videobox component

---
+++ tests/unit/components/video-box/VideoBox.spec.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { render, screen } from '@testing-library/react'
+import '@testing-library/jest-dom/extend-expect'
+import VideoBox from '~/components/video-box/VideoBox'
+
+describe('VideoBox component', () => {
+  it('renders the video component', () => {
+    const videoUrl = '~/assets/img/guest-home-page/videoImg.png'
+    render(<VideoBox video={videoUrl} />)
+    const videoBoxElement = screen.getByTestId('videoBox')
+    const videoElement = screen.getByAltText('Video')
+    expect(videoBoxElement).toBeInTheDocument()
+    expect(videoElement).toBeInTheDocument()
+  })
+})
