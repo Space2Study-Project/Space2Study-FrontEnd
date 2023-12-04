e47f31f6ac3d3505126a368659c7980e241d6c03
Andrii Nasadiuk <123100628+AndriiNasadiuk@users.noreply.github.com>
2023-12-04 16:08:37 +0200

    Added logic to Login btn

--- src/containers/guest-home-page/login-form/LoginForm.jsx
+++ src/containers/guest-home-page/login-form/LoginForm.jsx
@@ -71,6 +71,7 @@ const LoginForm = ({
       </Typography>
 
       <AppButton
+        disabled={t(!errors.email) && data.password ? false : true}
         loading={authLoading}
         size='large'
         sx={styles.loginButton}
