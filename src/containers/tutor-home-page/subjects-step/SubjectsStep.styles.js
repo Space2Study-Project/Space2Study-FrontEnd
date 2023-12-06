f1ddeec51b70d80fa12e357060312a5568186e36
marianamartsy <mart7mariana@gmail.com>
2023-12-06 17:58:13 +0100

    SubjectsStep container added

--- src/containers/tutor-home-page/subjects-step/SubjectsStep.styles.js
+++ src/containers/tutor-home-page/subjects-step/SubjectsStep.styles.js
@@ -3,9 +3,8 @@ import { fadeAnimation } from '~/styles/app-theme/custom-animations'
 export const styles = {
   container: {
     display: 'flex',
-    // justifyContent: 'space-between',
-    // gap: { md: '0px', lg: '60px' },
-    gap: '40px',
+    justifyContent: 'space-between',
+    gap: { md: '25px', lg: '80px' },
     height: { sm: '485px' },
     paddingBottom: { xs: '30px', sm: '0px' },
     flexDirection: { xs: 'column', md: 'row' },
@@ -13,22 +12,23 @@ export const styles = {
   },
   imgContainer: {
     display: 'flex',
-    width: '50%',
+    alignItems: 'flex-start',
+    width: { sm: '100%', md: '38%', lg: '50%' },
     maxWidth: { md: '370px', lg: '472px' },
     maxHeight: 'inherit'
   },
   img: {
     objectFit: 'contain',
-    width: '80%',
+    width: '100%',
     marginBottom: '30px'
   },
   inputContainer: {
     display: 'flex',
     flexDirection: 'column',
-    maxWidth: { md: '350px', lg: '455px' }
+    width: { sm: '100%', md: '100%', lg: '100%' },
+    maxWidth: { md: '430px', lg: '455px' }
   },
   btnsBox: {
-    alignItems: 'flex-end',
     marginTop: 'auto'
   },
   appButton: {
