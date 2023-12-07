a1b0e3aaa2f54f143c3ae0d8fb5839772eec275a
Leonid <leonideko1@gmail.com>
2023-12-07 21:49:54 +0200

    return previus style

--- src/containers/tutor-home-page/general-info-step/GeneralInfoStep.styles.js
+++ src/containers/tutor-home-page/general-info-step/GeneralInfoStep.styles.js
@@ -3,28 +3,9 @@ import { fadeAnimation } from '~/styles/app-theme/custom-animations'
 export const styles = {
   container: {
     display: 'flex',
-    justifyContent: 'space-between',
+    justifyContent: 'space-around',
     height: { sm: '485px' },
     paddingBottom: { xs: '30px', sm: '0' },
     ...fadeAnimation
-  },
-  form: {
-    display: 'flex',
-    flexDirection: 'column',
-    width: '430px',
-    gap: '20px'
-  },
-  appearance: {
-    display: 'flex',
-    justifyContent: 'space-between'
-  },
-  textField: {
-    width: '48%'
-  },
-  selects: {
-    width: '48%'
-  },
-  description: {
-    width: '100%'
   }
 }
