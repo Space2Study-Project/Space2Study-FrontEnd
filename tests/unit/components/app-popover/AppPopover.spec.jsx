819376687c508fdae6554d774d8abd4734250d0a
marianamartsy <mart7mariana@gmail.com>
2023-12-14 11:20:58 +0100

    edited

--- tests/unit/components/app-popover/AppPopover.spec.jsx
+++ tests/unit/components/app-popover/AppPopover.spec.jsx
@@ -1,54 +1,71 @@
 import { render, fireEvent, waitFor } from '@testing-library/react'
-
 import AppPopover from '~/components/app-popover/AppPopover'
+import { beforeEach, vi } from 'vitest'
+
+const onClose = vi.fn()
+const onClick = vi.fn()
+
+// vi.mock('@mui/material/Popover', () => ({
+//   __esModule: true,
+//   default: function () {
+//     return <div onClose={onClose} data-testid={'app-popover'}></div>
+//   }
+// }))
 
 const initialItems = <div data-testid='initial-items'>Initial Items</div>
-const showMoreElem = <div data-testid='show-more'>Show More</div>
-
+const showMoreElem = (
+  <div data-testid='show-more' onClick={onClick}>
+    Show More
+  </div>
+)
 describe('AppPopover component', () => {
-  it('renders initial items and showMoreElem', () => {
-    const { getByTestId } = render(
+  let getByTestId
+
+  beforeEach(() => {
+    const rendered = render(
       <AppPopover initialItems={initialItems} showMoreElem={showMoreElem} />
     )
+    getByTestId = rendered.getByTestId
+  })
 
+  it('renders initial items and showMoreElem', () => {
     expect(getByTestId('initial-items')).toBeInTheDocument()
     expect(getByTestId('show-more')).toBeInTheDocument()
   })
 
   it('opens popover when showMoreElem is clicked', () => {
-    const { getByTestId } = render(
-      <AppPopover initialItems={initialItems} showMoreElem={showMoreElem} />
-    )
-
     fireEvent.click(getByTestId('show-more'))
-
+    expect(onClick).toHaveBeenCalled()
     expect(getByTestId('app-popover')).toBeInTheDocument()
   })
 
-  it('hides element when hideElem is true and popover is open', () => {
-    const { getByTestId, getByText } = render(
-      <AppPopover
-        hideElem
-        initialItems={initialItems}
-        showMoreElem={showMoreElem}
-      />
-    )
-
-    fireEvent.click(getByTestId('show-more'))
-
-    const initialItemsWrapper = getByText('Initial Items').parentElement
-    expect(initialItemsWrapper).toHaveStyle('visibility: hidden')
-  })
-
   it('closes popover when clicking outside', () => {
-    const { getByTestId } = render(
-      <AppPopover initialItems={initialItems} showMoreElem={showMoreElem} />
-    )
     waitFor(() => {
       fireEvent.click(getByTestId('show-more'))
       fireEvent.click(document)
 
+      expect(onClose).toHaveBeenCalled()
       expect(getByTestId('app-popover')).not.toBeInTheDocument()
     })
+  })
+
+  it('triggers openPopover function when clicking on the popover', async () => {
+    fireEvent.click(getByTestId('show-more'))
+
+    const popover = getByTestId('app-popover')
+    expect(popover).toBeInTheDocument()
+
+    fireEvent.click(popover)
+
+    await waitFor(() => {
+      expect(popover).toBeInTheDocument()
+    })
+  })
+
+  it('triggers onClick handler when showMoreElem is clicked', () => {
+    fireEvent.click(getByTestId('show-more'))
+
+    expect(onClick).toHaveBeenCalled()
+    expect(getByTestId('app-popover')).toBeInTheDocument()
   })
 })
