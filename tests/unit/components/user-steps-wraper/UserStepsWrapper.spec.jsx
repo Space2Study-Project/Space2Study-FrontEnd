79129443c97f008fcbd65ecac70b0c86ab149c49
marianamartsy <mart7mariana@gmail.com>
2023-12-17 14:17:24 +0100

    UserStepsWrapper.spec tests edited

--- tests/unit/components/user-steps-wraper/UserStepsWrapper.spec.jsx
+++ tests/unit/components/user-steps-wraper/UserStepsWrapper.spec.jsx
@@ -1,5 +1,5 @@
 import { vi } from 'vitest'
-import { render, screen, waitFor } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 import UserStepsWrapper from '~/components/user-steps-wrapper/UserStepsWrapper'
 
 const mockDispatch = vi.fn()
@@ -13,11 +13,45 @@ vi.mock('react-redux', async () => {
     useSelector: () => mockSelector
   }
 })
+vi.mock('~/components/step-wrapper/StepWrapper', () => ({
+  __esModule: true,
+  default: ({ children }) => (
+    <div data-testid='mocked-step-wrapper'>{children}</div>
+  )
+}))
+
+vi.mock('~/redux/reducer', () => ({
+  markFirstLoginComplete: vi.fn()
+}))
+
+vi.mock('~/context/step-context', () => ({
+  StepProvider: ({ children }) => <div>{children}</div>
+}))
+
+vi.mock('~/containers/tutor-home-page/add-photo-step/AddPhotoStep', () => ({
+  __esModule: true,
+  default: () => <div data-testid='AddPhotoStep' />
+}))
+vi.mock(
+  '~/containers/tutor-home-page/general-info-step/GeneralInfoStep',
+  () => ({
+    __esModule: true,
+    default: () => <div data-testid='GeneralInfoStep' />
+  })
+)
+vi.mock('~/containers/tutor-home-page/language-step/LanguageStep', () => ({
+  __esModule: true,
+  default: () => <div data-testid='LanguageStep' />
+}))
+vi.mock('~/containers/tutor-home-page/subjects-step/SubjectsStep', () => ({
+  __esModule: true,
+  default: () => <div data-testid='SubjectsStep' />
+}))
+
 describe('UserStepsWrapper Component', () => {
   it('renders UserStepsWrapper component', () => {
     render(<UserStepsWrapper userRole='student' />)
-    waitFor(() => {
-      expect(screen.getByTestId('nameInputs')).toBeInTheDocument()
-    })
+
+    expect(screen.getByTestId('GeneralInfoStep')).toBeInTheDocument()
   })
 })
